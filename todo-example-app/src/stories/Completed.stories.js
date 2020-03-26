d011df8db858ca188dbfac11416335bdaa90ae03
Abby Gottlich <abby.gottlich@gmail.com>
2020-03-26 10:42:30 -0500

    new commit

--- todo-example-app/src/stories/Completed.stories.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { storiesOf } from "@storybook/react";
-import ShowCompleted from "../components/Completed";
-
-storiesOf("Completed", module).add("default view", () => {
-  return <ShowCompleted />;
-});
